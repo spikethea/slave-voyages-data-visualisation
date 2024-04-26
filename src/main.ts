889fefeb140748d6cb5d061de78114f3325c0bed
Spikethea <spike00890@hotmail.co.uk>
2024-04-26 12:23:00 +0100

    Article format

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,6 @@ import {setupScatterChart} from './scatter-chart.ts'
 import {setupPieChart} from './pie-chart.ts'
 //import setupTreeMap from './tree-map.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    
-    <h1>Slave Voyage Vizualisation</h1>
-    <div class="card">
-      <div id="scatter-chart"></div>
-    </div>
-      <div id="pie-chart"></div>
-      <div class="">
-        <input type="range" min="1" max="100" value="50" class="piechart-slider" id="myRange">
-      </div>
-    <div class="card">
-      <div id="treemap"></div>
-    </div>
-    <p class="read-the-docs">
-      A interactive data visualisation of the many voyages from europe to the americas in the transatlantic slave trade.
-    </p>
-  </div>
-`
 
 setupScatterChart(document.querySelector<HTMLButtonElement>('#scatter-chart')!);
 setupPieChart(
