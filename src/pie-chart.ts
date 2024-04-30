31e17b07226268b8c3e6e1faaedafb653fa7ded9
Spikethea <spike00890@hotmail.co.uk>
2024-04-30 16:26:49 +0100

    data viz finished (no animations/transitions)

--- src/pie-chart.ts
+++ src/pie-chart.ts
@@ -3,10 +3,16 @@ const filepath = "country-data.csv";
 
 export function setupPieChart(element: HTMLButtonElement, slider: HTMLInputElement) {
 
+
 let yearInput = 5;
 
+const countryLabel = document.createElement('h4');
 const yearLabel = document.createElement('div');
+
+countryLabel.id = 'country-label';
 yearLabel.id = 'year-label';
+
+element.appendChild(countryLabel);
 element.appendChild(yearLabel)
 
 
@@ -25,6 +31,11 @@ const svg = d3.select(element)
     .attr("height", height)
   .append("g")
     .attr("transform", `translate(${width / 2},${height / 2})`);
+
+  // create reusable arc for fture use
+    const arc = d3.arc()
+    .innerRadius(150)// This is the size of the donut hole
+    .outerRadius(radius)
 
 // // Create dummy data
 // const data = {a: 9, b: 20, c:30, d:8, e:12}
@@ -62,7 +73,6 @@ const svg = d3.select(element)
     const pie = d3.pie()
       .value(function (d) {
         if (d[0] !== "" && d[0] !== "Totals") {
-          console.log(d);
           return d[1];
         } else return;
         
@@ -70,13 +80,21 @@ const svg = d3.select(element)
       .sort((a, b) => Number(a[1]) - Number(b[1]))
 
     const data_ready = pie(Object.entries(data[yearIndex]));
-
+    
     let local = d3.local()
     
 
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
     const u = svg.selectAll("path")
       .data(data_ready)
+      
+    // svg.on('mouseover', function (event, d) {
+    //   d3.select(this).transition()
+    //            .duration(50)
+    //            .attr('opacity', '.85');
+    //     countryLabel.textContent = `${d.value}`;
+    //     console.log(d)
+    // })
 
 
       u
@@ -88,29 +106,45 @@ const svg = d3.select(element)
       .each(function(d) {
         local.set(this, d)
       })
-      .transition()
-      .duration(1000)
-      .attrTween('d', function(d) {
-        let i = d3.interpolate(local.get(this), d);
-        local.set(this, i(0));
-        return function(t) {
-          return arc(i(t));
-        }
-      })
-      .attr('d', d3.arc()
-        .innerRadius(150)         // This is the size of the donut hole
-        .outerRadius(radius)
+      // .transition()
+      // .duration(1000)
+      // .attrTween('d', function(d) {
+      //   let i = d3.interpolate(local.get(this), d);
+      //   local.set(this, i(0));
+      //   return function(t) {
+      //     return arc(i(t));
+      //   }
+      // })
+      .attr('d', arc
       )
       .attr('fill', colorScale)
       .attr("stroke", "black")
       .style("stroke-width", "2px")
       .style("opacity", 0.7)
 
-      u
+
+      svg
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .attr("text-anchor", "middle")
+      .selectAll('text')
+      
+      .data(data_ready)
+      .join(
+        enter => enter.append("text"),
+        update => update,
+        exit => exit.remove()
+      )
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .text(d => d.data[0])
+
+    u
     .exit()
     .remove()
 
       yearLabel.innerHTML = data[yearIndex][''];
+
+      return svg.node();
   }
 
 }
